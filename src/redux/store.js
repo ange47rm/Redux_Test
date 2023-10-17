2684795cb0802082f791c63f9225f1a587ce5983
Angelo Di Massimo <angelo.dimassimo@mrmglobal.com>
2023-10-17 16:16:38 +0100

    Added change listener

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from './counter'
 
 // 1. Configure store (there can be more than one store, based on what's being stored)
-export default configureStore({
+const store = configureStore({
 
     // 2. Import and add reducer
     reducer: {
         counter: counterReducer
     }
 });
+
+// Example listener function
+const onStoreUpdated = () => {
+    console.log('The store has been updated. Current store state:', JSON.stringify(store.getState()))
+}
+
+// Subscribe change listener function to store
+store.subscribe(onStoreUpdated)
+
+export default store;
